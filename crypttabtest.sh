581f3cf7fe2ede9228beb5bc338e430622fe5245
Philipp Schmitt <philipp@schmitt.co>
2016-06-22 09:44:04 +0200

    Add comments

--- crypttabtest.sh
+++ crypttabtest.sh
@@ -5,12 +5,15 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+# Get crypttab content, without commented or empty lines
 grep -Ev '^$|^#' /etc/crypttab | while read name device password options
 do
+    # If password is a file assume it is a key file
     if [[ -r "$password" ]]
     then
         cryptsetup luksOpen "$device" --key-file "$password" "$name"
     else
+        # Unlock using plaintext password
         echo "$password" | cryptsetup luksOpen "$device" "$name"
     fi
 done
